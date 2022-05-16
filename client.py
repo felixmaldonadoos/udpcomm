53c55caa4bbd37f35b09f71efe7bab5473dc120a
felixmaldonado <75142964+felixmaldonadoos@users.noreply.github.com>
2022-05-16 18:51:02 -0400

    move

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ import re
 import time
 
 # read file to extract IP and connections
-with open('address.txt') as fh:
+with open('address/address_client.txt') as fh:
     fstring = fh.readlines()
 # declaring the regex pattern for IP addresses
 pattern_ip = re.compile(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
