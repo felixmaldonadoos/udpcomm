ad8125b80d2c90cee37022c6643cfa827dbd134d
felixmaldonado <75142964+felixmaldonadoos@users.noreply.github.com>
2022-05-16 17:51:05 -0400

    changes

--- server.py
+++ server.py
@@ -2,12 +2,16 @@ import socket
 import sys
 import re
 
+# read file to extract IP and connections
+with open('address.txt') as fh:
+    fstring = fh.readlines()
 # declaring the regex pattern for IP addresses
 pattern_ip = re.compile(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
 # initializing the list object
 UDP_IP = pattern_ip.search(fstring[0])[0] # find ip
 UDP_PORT = int(re.findall('[0-9]+', fstring[1])[0]) # find port num
 # Create a UDP socket
+print(f"IP: {UDP_IP}\nPort: {UDP_PORT}" )
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # Bind the socket to the port
 server_address = (UDP_IP, UDP_PORT)
@@ -17,7 +21,8 @@ print("Do Ctrl+c to exit the program !!")
 while True:
     print("####### Server is listening #######")
     data, address = s.recvfrom(4096)
+    # print recvd data
     print("\n\n 2. Server received: ", data.decode('utf-8'), "\n\n")
-    send_data = input("Type some text to send => ")
-    s.sendto(send_data.encode('utf-8'), address)
-    print("\n\n 1. Server sent : ", send_data,"\n\n")
+    # send back same packet to confirm. 
+    s.sendto(data, address)
+    print("\n\n 1. Server sent : ", data,"\n\n")
